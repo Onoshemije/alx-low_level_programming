1c3a6964e21e53d21b87c7e9dcb8ee4799edc481
Onoshemije <lawani.precious2@gmail.com>
2022-09-09 21:16:04 -0700

    positive

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -15,10 +15,10 @@ int main(void)
 
 		n = rand() - RAND_MAX / 2;
 		if (n > 0)
-		printf("%n is positive\n" n);
+		printf("%n is positive\n");
 		else if (n == 0)
-		printf("%n is zero\n" n);
+		printf("%n is zero\n");
 		else if (n < 0)
-		printf("%n is negative\n" n);
+		printf("%n is negative\n");
 		return (0);
 }
