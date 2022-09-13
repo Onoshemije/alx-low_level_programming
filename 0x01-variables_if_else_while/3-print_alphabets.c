583e6595c2cee1f6e4f6c917e2731071e3b05fea
Onoshemije <lawani.precious2@gmail.com>
2022-09-12 18:07:11 -0700

    uppercase

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - putchar
+ *
+ * Description: prints the alphabet in lowercase, and then in uppercase
+ * Return: 0
+ */
+int main(void)
+{
+	char ch;
+
+for (ch = 'a'; ch <= 'z' ; ch++)
+}
+	putchar(ch);
+{
+	putchar (‘\n’);
+}
+	for (ch = 'A' ; ch <= 'Z' ; ch++)
+{
+	putchar(ch);
+}
+	putchar (‘\n’);
+	return (0);
+}
