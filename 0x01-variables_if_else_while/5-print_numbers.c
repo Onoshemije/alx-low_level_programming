65148d539c4c15d57b9c36ef0602d4855453c02f
Onoshemije <lawani.precious2@gmail.com>
2022-09-12 20:07:56 -0700

    digits

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio>
+/**
+ * main - digit
+ *
+ * Description: program that prints all single digit numbers of base
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%i", i);
+	}
+	printf("\n");
+	return (0);
+}
