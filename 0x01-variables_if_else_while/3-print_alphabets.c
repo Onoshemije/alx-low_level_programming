dd0b75cf7556e8159a284809175f4d3d2e80ad41
Onoshemije <lawani.precious2@gmail.com>
2022-09-12 19:09:00 -0700

    alphsBET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,13 +9,14 @@ int main(void)
 {
 	char ch;
 
-for (ch = 'a'; ch <= 'z' ; ch++)
-
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+{
 	putchar(ch);
 }
-	for (ch = 'A' ; ch <= 'Z' ; ch++)
-	putchar(ch);
+for (ch = 'A' ; ch <= 'Z' ; ch++)
 {
-	putchar ('\n');
+	putchar(ch);
+}
+	putchar('\n');
 	return (0);
 }
