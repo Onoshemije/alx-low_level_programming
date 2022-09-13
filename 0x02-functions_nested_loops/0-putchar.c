f74fda4f03c6b25b1b52c41d97ceb6c2ebff7fef
Onoshemije <lawani.precious2@gmail.com>
2022-09-13 13:19:03 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - putchar
+ * Description: program prints putchar
+ * Return: 0
+ */
+int main(void)
+{
+	char c;
+
+	printf("_putchar");
+	{
+	printf("\n");
+	return (0);
+}
