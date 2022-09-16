767893cbaabb49e52991112dc46836c1a00f44b1
Onoshemije <lawani.precious2@gmail.com>
2022-09-15 22:05:01 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,18 +12,28 @@ int main(void)
 {
 	int 1;
 
-	for (i = 1; i <= 100; i++)
-	{
-		if (i % 15 == 0)
+	for (i = 1; i <= 100; i++)	
+	{	
+		if (i % 3 == 0 && i % 5 != 0)
+		{
+			printf("Fizz");
+		}
+		else if (i % 5 == 0 && i % 3 != 0)
+		{
+			printf("Buzz");
+		}
+		else if (i % 3 == 0 && i % 5 == 0)
+		{
 			printf("FizzBuzz");
-		else if (i % 3 == 0)
-				printf("Fizz");
-			else if (i % 5 == 0)
-				 printf("Buzz");
-			else
-				printf("%i", i);
-			if (i < 100)
-				printf(" ");
+		}
+		else if (i == 1)
+		{
+			printf("%d", i);
+		}
+		else
+			{
+				printf(" %d", i);
+			}
 		}
 		printf("\n");
 		return (0);
