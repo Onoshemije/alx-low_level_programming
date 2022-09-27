7ba39842082d720d4800c199c71c7efec669aeed
Onoshemije <lawani.precious2@gmail.com>
2022-09-26 18:53:45 -0700

    8-print_diagsums.c

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -14,8 +14,6 @@ void print_diagsums(int *a, int size)
 
 	for (i = 0; i <= (size * size); i = i + size + 1)
 		total1 = total1 + a[i];
-
-	
 	for (n = size - 1; n <= (size * size) - size; n = n + size - 1)
 		total2 = total2 + a[n];
 	printf("%d, %d\n", total1, total2);
