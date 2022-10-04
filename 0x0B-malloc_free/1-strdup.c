d5bc908d59eff26a7695f55a3699e308cf726ca8
Onoshemije <lawani.precious2@gmail.com>
2022-10-04 12:30:29 -0700

    1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -19,8 +19,8 @@ char *_strdup(char *str)
 		return (NULL);
 
 	for (index = 0; str[index]; index++)
-		len++
-			cpy = malloc(sizeof(char) * (len + 1));
+		len++;
+	cpy = malloc(sizeof(char) * (len + 1));
 
 	if (cpy == NULL)
 		return (NULL);
