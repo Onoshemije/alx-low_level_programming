271104f46c281ceb5922664e05a82b74a73cf5cf
Onoshemije <lawani.precious2@gmail.com>
2022-10-25 15:53:31 -0700

    pop

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * pop_listint - Entry Point
+ * @head: head
+ *
+ */
+int pop_listint(listint_t **head)
+{
+	listint_t *tmp;
+	int ret;
+
+	if (*head == NULL)
+		return (0);
+
+	tmp = *head;
+	ret = (*head)->n;
+	*head = (*head)->next;
+
+	free(tmp);
+
+	return (ret);
+}
